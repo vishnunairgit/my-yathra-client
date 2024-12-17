1f36d838604a7174b4704216b8045e7f768f1cfa
vishnu nair <infovishnunair@gmail.com>
2024-12-17 14:23:07 +0530

    17-2024

--- src/Api/BookNow.js
+++ src/Api/BookNow.js
@@ -1,18 +1,5 @@
 import AxiosInstance from "../Config/AxiosInstance";
 
-// export const BookNow = async (booknow) => {
-//     try {
-//         const response = await AxiosInstance.post('/BookNow', booknow, {
-//             headers: {
-//                 'Content-Type': 'application/json', // Specify the payload is JSON
-//             }, 
-//         });
-//         return response.data;
-//     } catch (error) {
-//         console.error("Error while adding the trip:", error);
-//         throw error;
-//     }
-// };
 
 export const BookNow = async (booknow) => {
     try {
